c77ab86793914c6e583d1d0d84e5aca91e38ec6f
Bruno Cesar <brunocesarferreira1999@gmail.com>
2026-04-05 21:48:25 -0300

    Update run_cmd.py

--- run_cmd.py
+++ run_cmd.py
@@ -9,3 +9,13 @@ subprocess.run([
 	"--time",
 	"500",
 ], check=True)
+
+  File "C:\Users\bcfsant\www\ncc-test\run_cmd.py", line 4, in <module>
+    subprocess.run([
+  File "C:\Program Files\Python310\lib\subprocess.py", line 501, in run
+    with Popen(*popenargs, **kwargs) as process:
+  File "C:\Program Files\Python310\lib\subprocess.py", line 966, in __init__
+    self._execute_child(args, executable, preexec_fn, close_fds,
+  File "C:\Program Files\Python310\lib\subprocess.py", line 1435, in _execute_child
+    hp, ht, pid, tid = _winapi.CreateProcess(executable, args,
+OSError: [WinError 1392] The file or directory is corrupted and unreadable
