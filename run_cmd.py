9a76c66d2b8dfab287242508face75deb21f1344
Bruno Cesar <brunocesarferreira1999@gmail.com>
2026-04-05 21:47:49 -0300

    Update run_cmd.py

--- run_cmd.py
+++ run_cmd.py
@@ -1,2 +1,11 @@
 import subprocess
-subprocess.run(r"C:\Users\bruno\Desktop\ncc-atual\genetic_nesting_optimized.exe")
+# subprocess.run(r"C:\Users\bcfsant\www\ncc-test\genetic_nesting_optimized.exe")
+
+subprocess.run([
+	"nesting.exe",
+	"input_shapes.json",
+	"-o",
+	"genetic_nesting_optimized_result.json",
+	"--time",
+	"500",
+], check=True)
